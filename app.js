83783778097ad51d0380aa6b6f7aa374e3a5a3df
queuecat <2356583659@qq.com>
2022-07-24 18:56:39 +0800

    feat: 监听https

--- app.js
+++ app.js
@@ -8,24 +8,33 @@ const path = require('path');
 const fs = require('fs');
 //引入http模块
 const http = require('http');
-// const https = require('https');
-//配置https对象
-const httpServer = http.createServer(app);
+const https = require('https');
 
+//配置https对象
+// const options = {
+//     key: fs.readFileSync(path.join(__dirname, '../https', 'queuecat.top.key')),
+//     cert: fs.readFileSync(path.join(__dirname, '../https', 'queuecat.top.pem'))
+// };
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({}, app);
 
 // 一级路由
 const router = {
-	api: require("./router/api"),
-	intercept: require("./router/intercept"),
+	api: require('./router/api'),
+	intercept: require('./router/intercept'),
 };
 
 // 静态资源托管
 app.use(express.static(path.join(__dirname, './public')));
 // 抓包接口
-app.use('/api',router.api)
+app.use('/api', router.api);
 // 拦截
 app.use(router.intercept);
 
 httpServer.listen(3000, () =>
 	console.log('\033[42;30m 成功 \033[0m', '服务器启动成功')
+);
+httpsServer.listen(443, () =>
+	console.log('\033[42;30m 成功 \033[0m', '服务器启动成功 on port 443')
 );
--- router/intercept.js
+++ router/intercept.js
@@ -1,6 +1,8 @@
 // 引用express框架
 const express = require('express');
-const axios = require('axios');
+// const axios = require('axios');
+const request = require('request');
+const http = require('http');
 
 const intercept = express.Router();
 
@@ -8,16 +10,25 @@ intercept.use((req, res) => {
 	const url = req.url;
 	const method = req.method;
 	console.log(url);
-	// res.send('1');
-	axios(req)
-		.then((data) => {
-			console.log(Object.keys(data));
-			console.log('data.data', data.data);
-			res.send(data);
-		})
-		.catch((err) => {
-			console.log('err', err.message);
-		});
+	res.send('1');
+	// axios(req)
+	// 	.then((data) => {
+	// 		// console.log(Object.keys(data));
+	// 		// console.log('data.data', data.data);
+	// 		// res.send(data.data);
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log('err', err.message);
+	// 	});
+	// request(req,(error, response, body)=>{
+	//   console.log(error);
+	// })
+	if (req.secure) {
+		console.log(req.secure,url);
+    
+	} else {
+		console.log(req.secure,url);
+	}
 });
 
 //导出路由对象
